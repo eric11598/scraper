504a680b797425c5e37640a910f9316a78242c71
eric11598 <eric.arellano.0803@gmail.com>
2018-10-25 01:48:03 -0400

    mongodb

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,7 +11,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populate
 
 mongoose.connect(MONGODB_URI);
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 
 // Routes
